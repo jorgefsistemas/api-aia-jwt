6d769ed17d5b2aae58d04d4971317c80e29351e7
jorgefsistemas <jorgefsistemas@gmail.com>
2022-10-23 22:09:24 -0500

    token expire

--- config/auth.php
+++ config/auth.php
@@ -90,8 +90,8 @@ return [
         'users' => [
             'provider' => 'users',
             'table' => 'password_resets',
-            'expire' => 60,
-            'throttle' => 60,
+            'expire' => 600,
+            'throttle' => 600,
         ],
     ],
 
